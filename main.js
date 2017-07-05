6ebc02930bf62b29dc31fd8a5bde81b923295d30
sars001x <sars001x@gmail.com>
2017-07-05 15:06:45 +0800

    Component exercise message

---
+++ main.js
@@ -0,0 +1,16 @@
+Vue.component('message' , {
+    props:['title' , 'body'],
+    template:
+    '<article class="message"> ' +
+    '<div class="message-header"> ' +
+    '  {{title}} ' +
+    '</div> ' +
+    '<div class="message-body"> ' +
+    '{{body}}' +
+    '</div> ' +
+    '</article>'
+});
+
+new Vue({
+    el:'#root'
+});
