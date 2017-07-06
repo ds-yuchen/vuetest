897f47e59272e990035c5389ebd915946d3ef3ce
sars001x <sars001x@gmail.com>
2017-07-06 16:55:07 +0800

    New Projecks

---
+++ hello-vue-sec/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <hello>HELLO THERE</hello>
+    <hello>HELLO THERE</hello>
+  </div>
+</template>
+
+<script>
+
+import Hello from './components/Hello.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+
+</style>
